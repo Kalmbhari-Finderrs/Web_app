d7c34bbfd307f6690bcb6d55803571681537d89a
mrugendrakul <mrugendraskulkarni@gmail.com>
2025-10-31 11:36:20 +0530

    - Installed router,tailwind package - Added routes for login_screen - Added LoginScreen only ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import LoginScreen from './Pages/LoginScreen'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<LoginScreen/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
